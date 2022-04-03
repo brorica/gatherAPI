17ab4e8e8efde9df2ae3ce01fcc4f7244c4d11c9
Brorica <minjea917@gmail.com>
2022-04-03 14:59:57 +0900

    first commit

---
+++ src/test/java/brorica/gather/GatherApplicationTests.java
@@ -0,0 +1,13 @@
+package brorica.gather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
